9cbf7f608b8a15f81801cba0c60566df31e36f47
STQT <productgsm@gmail.com>
2023-12-20 19:01:41 +0500

    task

--- lesson_9/classes.py
+++ lesson_9/classes.py
@@ -18,19 +18,46 @@ class Circle(Shape):
         self._pi = 3.14
         self._radius = radius
 
-    def area(self) -> int:
+    def area(self):
         return self._pi * self._radius * self._radius
+
+    def get_semi_area(self):
+        return "Половина площади круга равна: " + str(self.area() / 2)
 
     def get_area(self) -> str:
         return "Площадь круга: " + str(self.area())
 
-    def perimeter(self) -> int:
+    def perimeter(self):
         return 2 * self._pi * self._radius
 
     def get_perimeter(self) -> str:
         return "Длина окружности: " + str(self.perimeter())
 
+
 # circle = Circle(7)
+# print(circle.name)
+# print(circle.sides_number)
+# print(circle.sides)
+
+
+class Circle2(Shape):
+    def __init__(self, radius):
+        super().__init__(3, "Круг", radius)
+        self._pi = 3.14
+        self._radius = radius
+        self.area = self._pi * self._radius * self._radius
+        self.perimeter = 2 * self._pi * self._radius
+
+    def get_semi_area(self):
+        return "Половина площади круга равна: " + str(self.area / 2)
+
+    def get_area(self) -> str:
+        return "Площадь круга: " + str(self.area)
+
+    def get_perimeter(self) -> str:
+        return "Длина окружности: " + str(self.perimeter)
+
+# circle = Circle2(7)
 # print(circle.name)
 # print(circle.sides_number)
 # print(circle.sides)
