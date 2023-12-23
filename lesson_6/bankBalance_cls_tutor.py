99fa15bd2fdc9ef8a3b657c53d873d1a11d16f56
dzhamon <dzhamshed.amonov@ent-en.com>
2023-12-23 18:29:52 +0500

    Создание класса BankAccount и его подклассов

--- lesson_6/bankBalance_cls_tutor.py
+++ lesson_6/bankBalance_cls_tutor.py
@@ -1,39 +1,61 @@
+import pandas as pd
+
+pd.options.display.float_format = '{:,.2f}'.format
+
+
 class BankAccount:
-	def __init__(self, account_number, client_name, balance, amount):
-		self.account_number = account_number
-		self.client_name = client_name
-		self.balance = balance
-		self.amount = amount
+	def __init__(self, **kwargs):
+		self.account_number = kwargs.get('account_number', None)
+		self.client_name = kwargs.get('client_name', None)
+		self.balance = kwargs.get('balance', None)
+		self.amount = kwargs.get('amount', None)
 	
-	def deposit(self):
+	def make_deposit(self):
 		if self.amount > 0:
 			self.balance += self.amount
-			return f"Ваша сумма {self.amount} успешна зачислена на счет {self.account_number}"
-		
+			self.ansver = (f"{self.client_name}, Ваша сумма {self.amount} UZS "
+			               f"успешно зачислена на счет {self.account_number}. "
+			               f"Остаток счета = {self.balance} UZS")
+			return self.ansver
+	
 	def withdraw(self):
 		if self.balance >= self.amount:
 			self.balance -= self.amount
-			return f"Сумма {self.amount} успешно списана со счета (20406)  {self.account_number}"
+			return (f"{self.client_name}, cумма {self.amount} UZS успешно списана со счета {self.account_number}. "
+			        f"У Вас на депозите осталось {self.balance} UZS")
 		else:
-			print("Банк предлагает Вам воспользоваться услугой овердрафт")
+			return (f"Сумма списания  {self.amount} ' превышает остаток {self.balance} "
+			        f"Предлагаем воспользоваться услугой Овердрафт")
 
-			# (12503-Краткосрочные кредиты, предоставляемые физическим лицам
+# (12503-Краткосрочные кредиты, предоставляемые физическим лицам
 
-class SavingsAccount(BankAccount):
-	def __init__(self, interest_rate):
-		super().__init__(self.account_number, self.client_name, self.balance)
-		self.interest_rate = interest_rate
-	def add_interest(self):
-		self.balance += self.balance + (self.balance * self.interest_rate)/100
-		return f"Остаток на счете {self.account_number} сотавляет {self.balance}"
+
+class SavingAccount(BankAccount):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
 	
+	def add_interest(self, interest_rate=24):
+		self.balance += self.balance * interest_rate / 100
+		return f"С начисленными процентами остаток вашего счета составляет {self.balance}"
+
+
 class CurrentAccount(BankAccount):
-	def __init__(self, card_account):
-		super().__init__(self.account_number, self.client_name, self.balance, self.amount)
-		self.card_account = card_account
-	def check_balance(self):
-		return f"Здесь можно проверить остаток на крточном счет"
-	
-if __name__=='__main__':
-	pass
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
 
+	def add_sum_card(self, card_persent=12, card_sum=2400000):
+		card_sum += self.balance - self.balance * card_persent / 100
+		return f"{card_sum} - Это Ваша сумма на платиковой корточке"
+
+
+sarvar = BankAccount(kreditaccount='1250300397001', client_name='Sarvary', account_number='2020600397001',
+                     amount=2000000.00, balance=30000000.00)
+# print(sarvar.make_deposit())
+davron = BankAccount(client_name='Davron', account_number='2020600397301', amount=3000000, balance=25000000)
+print(CurrentAccount.add_sum_card(self=davron))
+dzhamon = BankAccount(client_name='Dzhamshed', account_number='2020400397001', amount=40000000.00, balance=30000000.00, card_sum=2400000)
+print(davron.withdraw())
+# artyom = BankAccount(client_name='Artyom', account_number='2020600397408', amount=12573000, balance=48000905)
+# print(artyom.withdraw())
+print(dzhamon.make_deposit())
+print(SavingAccount.add_interest(self=dzhamon))
