d2cb50014f8701f03a25ef802740d9ac383487d9
dzhamon <146824297+dzhamon@users.noreply.github.com>
2024-01-07 17:18:07 +0500

    Create del_dubls_massiv_v2.py
    вторая версия задачи из leetCode

---
+++ lesson_11/del_dubls_massiv_v2.py
@@ -0,0 +1,19 @@
+# Удаление из массива дублирующих элементов
+import random
+import numpy as np
+class Solution:
+    massiv = []
+    n = 0
+    while n != 15:
+        massiv.append(random.randint(2, 20))
+        n += 1
+    print(massiv)
+    sorted_mass = np.sort(massiv).tolist()
+    print(sorted_mass)
+    i = 0
+    while i <len(sorted_mass)-1:
+        if sorted_mass[i] != sorted_mass[i+1]:
+            i += 1
+        else:
+            del sorted_mass[i+1]
+    print(sorted_mass)
