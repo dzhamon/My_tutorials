9bbbb1dcfa33550616e75de65c488fc4054b917c
dzhamon <dzhamshed.amonov@ent-en.com>
2024-01-08 10:34:06 +0500

    Задача из leetCode

--- lesson_11/del_dubls_massiv.py
+++ lesson_11/del_dubls_massiv.py
@@ -1,25 +1,20 @@
 # Удаление дубликатов из отсортированного массива
+
 import random
 import numpy as np
 class Solution:
-	massiv = []
-	n = 0
-	while n != 15:
-		massiv.append(random.randint(2,20))
-		n += 1
-	sorted_mass = np.sort(massiv)
-	print(sorted_mass)
-	
-	def dim_reduction(self, my_array, k):
-		i = 0
-		while i < k:
-			if my_array[i] < my_array[i+1]:
-				i += 1
-			else:
-				array_temp = my_array.pop[i+1]
-				break
-		return array_temp
+    massiv = []
+    n = 0
+    while n != 15:
+        massiv.append(random.randint(2, 20))
+        n += 1
+    sorted_mass = np.sort(massiv).tolist()
+    print(sorted_mass)
 
-	while True:
-		dim_reduction(mass_copy, n)
-		
+    i = 0
+    while i <len(sorted_mass)-1:
+        if sorted_mass[i] != sorted_mass[i+1]:
+            i += 1
+        else:
+            del sorted_mass[i+1]
+    print(sorted_mass)
